e3b8cadcf497c319d5fb02b87768eb0e3fd07d69
alexandra andr <shura_1986@mail.ru>
2026-01-16 17:33:08 +0300

    documentation output is configured

--- config/settings.py
+++ config/settings.py
@@ -23,6 +23,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "rest_framework_simplejwt",
+    "drf_yasg",
 
     "users",
     "habits",
@@ -62,7 +63,7 @@ REST_FRAMEWORK = {
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
     "DEFAULT_PERMISSION_CLASSES": [
-        "rest_framework.permissions.IsAuthenticated",
+        'rest_framework.permissions.IsAuthenticated',
     ]
 }
 
